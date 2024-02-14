49bf08ba7e4f005dc0ec072afd3efa46030b5f1b
Ola Hällvall <ola.hallvall@gmail.com>
2024-02-14 17:13:15 +0100

    TransactGuard.AzureFunctions added

---
+++ TransactGuard.AzureFunctions/Function.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace TransactGuard.AzureFunctions
+{
+    public class Function
+    {
+        private readonly ILogger _logger;
+
+        public Function(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function>();
+        }
+
+        [Function("Function")]
+        public void Run([RabbitMQTrigger("TransactGuard.DetermineCompensation", ConnectionStringSetting = "http:::localhost:15672")] string myQueueItem)
+        {
+            _logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
