2fde9cb398498fa68c26a7b788eb8bf40da6f539
Ola Hällvall <ola.hallvall@gmail.com>
2024-03-26 16:23:34 +0100

    Send a messages

--- FlowDance.Client/RabbitMQUtils/Storage.cs
+++ FlowDance.Client/RabbitMQUtils/Storage.cs
@@ -1,10 +1,10 @@
 ﻿using FlowDance.Common.Events;
 using FlowDance.Common.Commands;
-using RabbitMQ.Client;
-using System.Collections.Generic;
-using System;
-using System.Threading.Channels;
-
+using RabbitMQ.Stream.Client; 
+using RabbitMQ.Stream.Client.Reliable;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text;
 
 namespace FlowDance.Client.RabbitMQUtils;
 
@@ -12,10 +12,10 @@ public class Storage
 {
     public void StoreEvent(Span span)
     {
-        string QueueName = span.TraceId.ToString();
+        var streamName = span.TraceId.ToString();
 
         //Check if stream/queue exist. 
-        if (QueueExist(QueueName))
+        if (StreamExist(streamName))
         {
             // Only first span in stream should be a root span.
             if (span is SpanOpened)
@@ -31,8 +31,25 @@ public class Storage
                 ((SpanOpened)span).IsRootSpan = true;
 
             // Create stream/queue
-            CreateQueue(QueueName);
+            CreateStream(streamName);
         }
+
+        var streamSystem = StreamSystem.Create(new StreamSystemConfig() 
+        {
+            UserName = "guest",
+            Password = "guest",
+            VirtualHost = "/",
+            Endpoints = new List<EndPoint>() { new IPEndPoint(IPAddress.Loopback, 5552) }
+        }, null).Result;
+
+        var confirmationTaskCompletionSource = new TaskCompletionSource<int>();
+        var producer = CreateProducer(streamName, streamSystem, confirmationTaskCompletionSource);
+
+        // Send a messages
+        producer.Send(new Message(Encoding.ASCII.GetBytes($"A"))).ConfigureAwait(false);
+
+        confirmationTaskCompletionSource.Task.Wait();
+        producer.Close().ConfigureAwait(false); 
     }
 
     public void StoreCommand(DetermineCompensation command)
@@ -40,12 +57,12 @@ public class Storage
 
     }
 
-    private bool QueueExist(string QueueName) 
+    private bool StreamExist(string streamName) 
     {  
         try {
             using var channel = SingletonConnection.getInstance().getConnection().CreateModel();
 
-            channel.QueueDeclarePassive(QueueName);  
+            channel.QueueDeclarePassive(streamName);  
         } 
         catch (RabbitMQ.Client.Exceptions.OperationInterruptedException ex) 
         {
@@ -62,14 +79,64 @@ public class Storage
         return true; 
     }
 
-    private void CreateQueue(string QueueName)
+    private void CreateStream(string streamName)
     {
         using var channel = SingletonConnection.getInstance().getConnection().CreateModel();
-        Dictionary<string, object> arguments = new Dictionary<string, object>
-        {
-            { "x-queue-type", "stream" }
-        };
+        Dictionary<string, object> arguments = new Dictionary<string, object> { { "x-queue-type", "stream" } };
 
-        channel.QueueDeclare(QueueName, true, false, false, arguments);
+        channel.QueueDeclare(streamName, true, false, false, arguments);
     }
+
+    private Producer CreateProducer(string StreamName, StreamSystem streamSystem, TaskCompletionSource<int> confirmationTaskCompletionSource)
+    {
+        var confirmationCount = 0;
+        const int MessageCount = 100;
+
+        var producer = Producer.Create(new ProducerConfig(streamSystem, StreamName)
+        {
+            ConfirmationHandler = async confirmation => 
+            {
+                Interlocked.Increment(ref confirmationCount);
+
+                // here you can handle the confirmation
+                switch (confirmation.Status)
+                {
+                    case ConfirmationStatus.Confirmed: 
+                        // all the messages received here are confirmed
+                        if (confirmationCount == MessageCount)
+                        {
+                            Console.WriteLine("*********************************");
+                            Console.WriteLine($"All the {MessageCount} messages are confirmed");
+                            Console.WriteLine("*********************************");
+                        }
+
+                        break;
+
+                    case ConfirmationStatus.StreamNotAvailable:
+                    case ConfirmationStatus.InternalError:
+                    case ConfirmationStatus.AccessRefused:
+                    case ConfirmationStatus.PreconditionFailed:
+                    case ConfirmationStatus.PublisherDoesNotExist:
+                    case ConfirmationStatus.UndefinedError:
+                    case ConfirmationStatus.ClientTimeoutError:
+                        // (4)
+                        Console.WriteLine(
+                            $"Message {confirmation.PublishingId} failed with {confirmation.Status}");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                if (confirmationCount == MessageCount)
+                {
+                    confirmationTaskCompletionSource.SetResult(MessageCount);
+                }
+
+                await Task.CompletedTask.ConfigureAwait(false);
+            }
+        }, null).Result;
+
+        return producer;
+    }
+
 }
