22cae7edf96f0c390290f988d310978437c86e08
Ola Hällvall <ola.hallvall@gmail.com>
2024-03-26 11:31:34 +0100

    Singleton Connection support

---
+++ FlowDance.Client/RabbitMQUtils/SingletonConnection.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+
+namespace FlowDance.Client.RabbitMQUtils
+{
+    internal class SingletonConnection
+    {
+        private static SingletonConnection INSTANCE = new SingletonConnection();
+        private IConnection connection;
+        private SingletonConnection()
+        {
+            // here you can init your connection parameter
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            connection = factory.CreateConnection();
+        }
+
+        public static SingletonConnection getInstance()
+        {
+            return INSTANCE;
+        }
+
+        public IConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
--- FlowDance.Client/RabbitMQUtils/Storage.cs
+++ FlowDance.Client/RabbitMQUtils/Storage.cs
@@ -1,7 +1,9 @@
 ﻿using FlowDance.Common.Events;
 using FlowDance.Common.Commands;
 using RabbitMQ.Client;
+using System.Collections.Generic;
 using System;
+using System.Threading.Channels;
 
 
 namespace FlowDance.Client.RabbitMQUtils;
@@ -10,13 +12,26 @@ public class Storage
 {
     public void StoreEvent(Span span)
     {
-        if(span is SpanOpened)
+        string QueueName = span.TraceId.ToString();
+
+        //Check if stream/queue exist. 
+        if (QueueExist(QueueName))
         {
-            //Check if stream/queue exist. Only first span i stream should be a root span.
-            if (!QueueExist(span.TraceId.ToString())) 
-                ((SpanOpened)span).IsRootSpan = true;
-            else
+            // Only first span in stream should be a root span.
+            if (span is SpanOpened)
                 ((SpanOpened)span).IsRootSpan = false;
+        }
+        else
+        {
+            // SpanClosed should newer create the CreateQueue. Only SpanOpened are allowed to do that!  
+            if (span is SpanClosed)
+                throw new Exception("The event SpanClosed are trying to create a queue. This not allowed, only SpanOpened are allowed to do that!");
+
+            if (span is SpanOpened)
+                ((SpanOpened)span).IsRootSpan = true;
+
+            // Create stream/queue
+            CreateQueue(QueueName);
         }
     }
 
@@ -25,23 +40,36 @@ public class Storage
 
     }
 
-    public bool QueueExist(string QueueName) 
+    private bool QueueExist(string QueueName) 
     {  
         try {
-            var factory = new ConnectionFactory { HostName = "localhost" };
-            var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
+            using var channel = SingletonConnection.getInstance().getConnection().CreateModel();
 
             channel.QueueDeclarePassive(QueueName);  
         } 
         catch (RabbitMQ.Client.Exceptions.OperationInterruptedException ex) 
         {
-            return false; 
+            if(ex.Message.Contains("no queue"))
+                return false;
+            else
+                throw new Exception("Non suspected exception occurred. See inner exception for more details.", ex);
         }
         catch (Exception ex)
         {
-            throw new Exception("Non suspected exception occured. See inner exception for more info", ex);
+            throw new Exception("Non suspected exception occurred. See inner exception for more details.", ex);
         }
+
         return true; 
+    }
+
+    private void CreateQueue(string QueueName)
+    {
+        using var channel = SingletonConnection.getInstance().getConnection().CreateModel();
+        Dictionary<string, object> arguments = new Dictionary<string, object>
+        {
+            { "x-queue-type", "stream" }
+        };
+
+        channel.QueueDeclare(QueueName, true, false, false, arguments);
     }
 }
