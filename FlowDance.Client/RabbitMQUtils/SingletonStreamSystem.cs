db0eab07b6901d70b47276854c5894c173a27aca
Ola Hällvall <ola.hallvall@gmail.com>
2024-03-27 09:11:15 +0100

    Update SingletonStreamSystem.cs

--- FlowDance.Client/RabbitMQUtils/SingletonStreamSystem.cs
+++ FlowDance.Client/RabbitMQUtils/SingletonStreamSystem.cs
@@ -1,1 +1,31 @@
+﻿using RabbitMQ.Stream.Client; 
+using RabbitMQ.Stream.Client.Reliable;
 
+namespace FlowDance.Client.RabbitMQUtils
+{
+    internal class SingletonStreamSystem
+    {
+        private static SingletonStreamSystem INSTANCE = new SingletonStreamSystem();
+        private StreamSystem streamSystem;
+      
+        private SingletonStreamSystem()
+        {
+            streamSystem = StreamSystem.Create(new StreamSystemConfig() 
+            {
+                UserName = "guest",
+                Password = "guest",
+                Endpoints = new List<EndPoint>() {new IPEndPoint(IPAddress.Loopback, 5552)}
+            }, null).ConfigureAwait(false);
+        }
+
+        public static SingletonStreamSystem getInstance()
+        {
+            return INSTANCE;
+        }
+
+        public IConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
