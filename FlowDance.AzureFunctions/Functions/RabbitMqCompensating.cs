02d1a8a1004383aae2af12a7394e8678df553042
Ola Hällvall <ola.hallvall@gmail.com>
2024-05-23 15:28:30 +0200

    Update RabbitMqCompensating.cs

--- FlowDance.AzureFunctions/Functions/RabbitMqCompensating.cs
+++ FlowDance.AzureFunctions/Functions/RabbitMqCompensating.cs
@@ -16,56 +16,63 @@ namespace FlowDance.AzureFunctions.Functions
         {
         }
 
-        [Function(nameof(RabbitMqCompensating))]
+        [Function(nameof(RabbitMqCompensate))]
         public async Task<bool> RabbitMqCompensate([ActivityTrigger] string spanJson, FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("RabbitMqCompensating");
             var span = JsonConvert.DeserializeObject<Span>(spanJson, new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Auto });
             if (span == null)
             {
-                logger.LogError("There no Span data! The function HttpCompensate has noting to work with and will exit.");
-                throw new HttpRequestException("There no Span data! The function HttpCompensate has noting to work with and will exit.", null);
+                logger.LogError("There no Span data! The function RabbitMqCompensate has nothing to work with and will exit.");
+                throw new HttpRequestException("There no Span data! The function RabbitMqCompensate has nothing to work with and will exit.", null);
             }
-            var httpClient = _httpClientFactory.CreateClient();
+            
+            var config = new ConfigurationBuilder().AddJsonFile($"appsettings.json").Build();
+            var connectionFactory = new ConnectionFactory();
+            config.GetSection("RabbitMqConnection").Bind(connectionFactory);
 
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
             var compensatingAction = (AmqpCompensatingAction)span.SpanOpened.CompensatingAction;
-            var httpRequest = new HttpRequestMessage(HttpMethod.Post, compensatingAction.Url);
-
-            if (compensatingAction.PostData == null)
-                compensatingAction.PostData = JsonConvert.SerializeObject(span.TraceId.ToString());
-
-            // Set content/body
-            httpRequest.Content = new StringContent(compensatingAction.PostData, Encoding.UTF8, $"application/json");
-
+        
+            if (compensatingAction.MessageData == null)
+                compensatingAction.MessageData = span.TraceId.ToString();
+            
+            IBasicProperties props = channel.CreateBasicProperties();    
+            props.Headers = new Dictionary<string, object>();
+            
             // Set headers
+            // https://stackoverflow.com/questions/69877042/how-should-i-pass-a-string-value-in-a-rabbitmq-header
             if (compensatingAction.Headers != null)
             {
                 foreach (var header in compensatingAction.Headers)
                 {
-                    httpRequest.Headers.Authorization = new AuthenticationHeaderValue(header.Key, header.Value);
+                    props.Headers.Add(header.Key, header.Value);
                 }
 
                 // Always add a this headers
                 if (!compensatingAction.Headers.ContainsKey("x-correlation-id"))
-                    httpRequest.Headers.Authorization = new AuthenticationHeaderValue("x-correlation-id", span.TraceId.ToString());
+                    props.Headers.Add("x-correlation-id", span.TraceId.ToString());
 
                 if (!compensatingAction.Headers.ContainsKey("calling-function-name"))
-                    httpRequest.Headers.Authorization = new AuthenticationHeaderValue("calling-function-name", span.SpanOpened.CallingFunctionName);
-
+                    props.Headers.Add("calling-function-name", span.SpanOpened.CallingFunctionName);
             }
             else
             {
-                httpRequest.Headers.Authorization = new AuthenticationHeaderValue("x-correlation-id", span.TraceId.ToString());
-                httpRequest.Headers.Authorization = new AuthenticationHeaderValue("calling-function-name", span.SpanOpened.CallingFunctionName);
+                props.Headers.Add("x-correlation-id", span.TraceId.ToString());
+                props.Headers.Add("calling-function-name", span.SpanOpened.CallingFunctionName);
             }
 
-            // Send HTTP POST
-            var response = await httpClient.SendAsync(httpRequest).ConfigureAwait(false);
-            if (!response.IsSuccessStatusCode)
-            {
-                //throw new HttpRequestException(("A HTTP POST to {url} returns {statuscode}", compensatingAction.Url, response.StatusCode), null);
-                throw new HttpRequestException("A HTTP POST to {url} returns {statuscode}", null);
-            }
+            // So we can Confirm
+            channel.ConfirmSelect();
+
+            // Store the messages
+            channel.BasicPublish(exchange: string.Empty,
+                    routingKey: streamName,
+                    basicProperties: props,
+                    body: Encoding.Default.GetBytes(JsonConvert.SerializeObject(compensatingAction.MessageData));
+
+            channel.WaitForConfirmsOrDie(TimeSpan.FromSeconds(5));
          
             return true;
         }
