9a2465eb12d4e2470d4d96dfd5f4d1cf3aa658ca
Ola Hällvall <ola.hallvall@gmail.com>
2024-06-14 08:49:08 +0200

    New assembly for AspNetCore

---
+++ FlowDance.Client.AspNetCore/ActionFilters/CompensationSpanActionFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FlowDance.Client.AspNetCore.ActionFilters
+{
+    public class CompensationSpanActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+        }
+    }
+}
