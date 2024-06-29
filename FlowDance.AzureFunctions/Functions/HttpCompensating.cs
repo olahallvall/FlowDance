18cab583ae0565e3c5aba823724a95bb73bd6eb8
Ola Hällvall <ola.hallvall@gmail.com>
2024-06-29 10:15:49 +0200

    Fixed better error text

--- FlowDance.AzureFunctions/Functions/HttpCompensating.cs
+++ FlowDance.AzureFunctions/Functions/HttpCompensating.cs
@@ -65,9 +65,9 @@ namespace FlowDance.AzureFunctions.Functions
             if (!response.IsSuccessStatusCode)
             {  
                 // These StatusCode will not be retried.
-                if(response.StatusCode == System.Net.HttpStatusCode .Forbidden || response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                if(response.StatusCode == System.Net.HttpStatusCode.Forbidden || response.StatusCode == System.Net.HttpStatusCode.NotFound)
                 {
-                    logger.LogInformation("A HTTP POST to {url} returns {statuscode}. No retry!", compensatingAction.Url, response.StatusCode);
+                    logger.LogInformation("A HTTP POST to {url} returns {statuscode}. No retry will be performed for http codes 403 and 404", compensatingAction.Url, (int)response.StatusCode);
                     return false;
                 }
                     
